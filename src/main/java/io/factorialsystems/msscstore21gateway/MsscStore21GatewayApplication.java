17819490b25f6e1a238f256c0ff54ad99369e75f
Adebola Omoboya <adeomoboya@gmail.com>
2025-02-10 18:36:46 +0100

    First commit

---
+++ src/main/java/io/factorialsystems/msscstore21gateway/MsscStore21GatewayApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstore21gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStore21GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscStore21GatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/factorialsystems/msscstore21gateway/MsscStore21GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstore21gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscStore21GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
